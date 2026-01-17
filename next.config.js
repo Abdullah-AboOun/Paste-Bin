5578af11d8323b15319997c51a792b897412d8d5
Abdullah-AboOun <80777415+Abdullah-AboOun@users.noreply.github.com>
2026-01-17 10:57:58 +0000

    Add React Compiler to project
    - Install babel-plugin-react-compiler as dev dependency
    - Enable React Compiler in Next.js config
    - Compiler will run automatically in dev and production builds

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
 	output: "standalone",
+	experimental: {
+		reactCompiler: true,
+	},
 };
 
 export default config;
